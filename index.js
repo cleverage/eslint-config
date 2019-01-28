dfd4e60ccf94720cac5fe2d6f43ae548d87c7240
Yvain Liechti <yliechti@clever-age.com>
2019-01-28 15:11:29 +0100

    improve deps + lint itself + first travis config

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true,
-    "jest": true,
-    "browser": true,
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    browser: true,
   },
-  "parser": "babel-eslint",
-  "extends": "airbnb",
-  "plugins": [
-    "babel",
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  plugins: [
+    'babel',
   ],
-  "rules": {
-    "no-multiple-empty-lines": [1, { "max": 1, "maxEOF": 1, "maxBOF": 0 }],
-    "padding-line-between-statements": [
-        "error",
-        { "blankLine": "always", "prev": "*", "next": "return" },
-        { "blankLine": "always", "prev": "import", "next": "*" },
-        { "blankLine": "never", "prev": "import", "next": "import" },
-    ]
-  }
-}
+  rules: {
+    'no-multiple-empty-lines': [1, { max: 1, maxEOF: 1, maxBOF: 0 }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: 'import', next: '*' },
+      { blankLine: 'never', prev: 'import', next: 'import' },
+    ],
+  },
+};
